6bec8e0fe2d6901e6bfc714934231947a5968083
Hannahphoto <hannahprice67@gmail.com>
2024-03-08 15:16:23 -0500

    buttons for forms added

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-export default function SignUp({}){
+export default function SignUp({ }) {
     return (
         <>
             <h2>Sign-Up</h2>
@@ -7,21 +7,24 @@ export default function SignUp({}){
                     <div className="row">
                         <div className="input-field col s6">
                             <input placeholder="User Name" id="user_name" type="text" className="validate"></input>
-                                <label htmlFor="user_name">User Name</label>
+                            <label htmlFor="user_name">User Name</label>
                         </div>
                         <div className="row">
                             <div className="input-field col s12">
                                 <input id="password" type="password" className="validate"></input>
-                                    <label htmlFor="password">Password</label>
+                                <label htmlFor="password">Password</label>
                             </div>
                         </div>
                         <div className="row">
                             <div className="input-field col s12">
                                 <input id="email" type="email" className="validate"></input>
-                                    <label htmlFor="email">Email</label>
+                                <label htmlFor="email">Email</label>
                             </div>
                         </div>
                     </div>
+                    <button className="btn waves-effect waves-light" type="submit" name="action">SIGNUP
+                        <i className="material-icons right">send</i>
+                    </button>
                 </form>
             </div>
         </>
