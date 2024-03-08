c250f0d173a0da1186fd7fd527ef9a5f733e7612
Hannahphoto <hannahprice67@gmail.com>
2024-03-08 14:49:32 -0500

    pages made, signup and login configured, need to finish design, made media quiries for all dimensions from figma, added code to vite config file. note, there is an error asking for a server js file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
 import './styles/style.css'
-import './assets/css/materialize.min.css'
-import './index.html'
+
+import '/index.html'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Login from "../components/Login";
+import Footer from "../components/Footer";
+
+export default function LoginPage(){
+    return(
+        <div>
+            <Navbar/>
+            <Login/>
+            <Footer/>
+        </div>
+    )
+}
