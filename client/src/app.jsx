7911deaedf76ed15b43e15fe3d2267c2c61e81ef
Hannahphoto <hannahprice67@gmail.com>
2024-03-12 12:47:18 -0400

    fixed whatever was happening in navbar, added footer but need help will seek in class. gonna focus on phone plug in

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -8,7 +8,7 @@ function App() {
     // The Outlet component will conditionally swap between the different pages according to the URL
     return (
         <>
-            <Navbar />
+       
             <main id="main">
                 <Outlet />
             </main>
