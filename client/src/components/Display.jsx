47fbc442b3acc236ea9ac24090f1901fd187a980
Hannahphoto <hannahprice67@gmail.com>
2024-03-12 16:46:49 -0400

    started display page

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -1,3 +1,17 @@
-export default function Display(){
+export default function Display() {
+
+    return (
+        <>
+            <div className="petCircle"></div>
+            <div className="petStand"></div>
+            <div className="btnContainer">
+                <a className="waves-effect waves-light btn-small" id="greenColor">Play</a>
+                <a className="waves-effect waves-light btn-small" id="blueColor">Clean</a>
+                <a className="waves-effect waves-light btn-small" id="medPurpleColor">Sleep</a>
+                <a className="waves-effect waves-light btn-small" id="orangeColor">Feed</a>
+            </div>
+            <div className="healthContainer"></div>
+        </>
+    )
 
 }
