1db1980afdabb706e3fd42076babd35d43c4c6fa
mathilde-01 <mathilde.7892@gmail.com>
2024-03-14 19:09:50 -0400

    removed allUsers query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,29 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ALL_PROFILES = gql`
-  query allUsers {
-    users {
-      _id
-      name
-      email
-      pets {
-        _id
-        name
-        location
-        birthday
-        last_interaction
-        health {
-          _id
-          fun
-          cleanliness
-          hunger
-          sleep
-        }
-      }
-    }
-  }
-`;
-
 export const ME_QUERY = gql`
   query ME {
     me {
