f2b37e066fe664ddee600f46f92c539b0a60341f
Opal sn <59205540+OpalSnellneberger@users.noreply.github.com>
2024-03-14 19:32:04 -0400

    set basics for the front end of the mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,21 +1,40 @@
 import { gql } from "@apollo/client";
 
-// update health
+export const UPDATE_HEALTH_MUTATION = gql`
+  mutation UpdateHealth($healthId: ID!, $fun: Int!, $cleanliness: Int!, $hunger: Int!, $sleep: Int!) {
+    updateHealth(healthId: $healthId, fun: $fun, cleanliness: $cleanliness, hunger: $hunger, sleep: $sleep) {
+      _id
+      fun
+      cleanliness
+      hunger
+      sleep
+    }
+  }
+`;
 
-// update pet on load
+export const UPDATE_PET_MUTATION = gql`
+  mutation UpdatePetOnLoad($petId: ID!) {
+    updatePetOnLoad(petId: $petId) {
+      _id
+      // Include other fields you want to return after updating pet on load
+    }
+  }
+`;
 
-// delete pet
+export const DELETE_PET_MUTATION = gql`
+  mutation DeletePet($petId: ID!) {
+    removePet(petId: $petId) {
+      _id
+      // Include other fields you want to return after deleting the pet
+    }
+  }
+`;
 
-// signup
-
-export const LOGIN_MUTATION = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        email
-      }
+export const SIGNUP_MUTATION = gql`
+  mutation SignUp($name: String!, $email: String!) {
+    signUp(name: $name, email: $email) {
+      _id
+      email
     }
   }
 `;
