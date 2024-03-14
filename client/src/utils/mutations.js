d3f215699c24ac20964b4a2aa4aeb2f3c0a7f92e
scottd988 <>
2024-03-14 19:48:52 -0400

    added login utation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,17 @@
 import { gql } from "@apollo/client";
 
+export const LOGIN_MUTATION = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
+
 export const UPDATE_HEALTH_MUTATION = gql`
   mutation UpdateHealth($healthId: ID!, $fun: Int!, $cleanliness: Int!, $hunger: Int!, $sleep: Int!) {
     updateHealth(healthId: $healthId, fun: $fun, cleanliness: $cleanliness, hunger: $hunger, sleep: $sleep) {
