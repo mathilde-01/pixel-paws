976c58a463abe815df2a99ab1ee3195aa73252cb
Hannahphoto <hannahprice67@gmail.com>
2024-03-14 21:31:33 -0400

    change to flex-basis to change arrangment for dimensions, still working

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -5,22 +5,24 @@ export default function Display() {
     return (
         <>  <div className="displayContainer">
             <div className="petContainer ">
-            <div className="petCircle ">
-                <ThreeScene/>
-            </div>
-            <div className="petStand"></div>
+                <div className="petCircle ">
+                    <ThreeScene />
+                </div>
+                <div className="petStand"></div>
             </div>
             <div className="btnContainer">
                 <a className="waves-effect waves-light btn-small" id="greenColor">Play</a>
-           
+
                 <a className="waves-effect waves-light btn-small" id="blueColor">Clean</a>
-            
+
                 <a className="waves-effect waves-light btn-small" id="medPurpleColor">Sleep</a>
-        
+
                 <a className="waves-effect waves-light btn-small" id="orangeColor">Feed</a>
             </div>
-            <div className="healthContainer"></div>
+            <div className='mainHealthContainer'>
+                <div className="healthContainer"></div>
             </div>
+        </div>
         </>
     )
 
