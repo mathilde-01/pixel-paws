13063dd71797a40e719f3b9332fea26daf2c8243
Hannahphoto <hannahprice67@gmail.com>
2024-03-16 13:03:38 -0400

    footer contect changed, github repo link, hannah portfolio link, waiting on other porfolio links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,39 @@
-export default function Footer(){
-    return (
-        <footer className="page-footer">
-        <div className="container">
-          <div className="row">
-            <div className="col l6 s12">
-              <h5 className="white-text">Footer Content</h5>
+import {Link} from 'react-router-dom'
+
+export default function Footer() {
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
+    if(newWindow) newWindow.opener = null;
+  }
+  
+
+  return (
+    <footer className="page-footer">
+      <div className="container">
+        <div className="row">
+          {/* <div className="col l6 s12">
+              <h5 className="white-text">Meet the Team</h5>
               <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-            </div>
-            <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Links</h5>
-              <ul>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-              </ul>
-            </div>
+            </div> */}
+          <div className="col l4 offset-l2 s12">
+            <h5 className="white-text">Meet the Team</h5>
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="#!">Mathilde Alacron - Back End</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Diana Discher - Team Lead</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Scott Denonn - Back End</a></li>
+              <li><a className="grey-text text-lighten-3" href="#!">Opal Snellenberger - Back End</a></li>
+              <li><Link onClick={()=> openInNewTab("https://hprice-web-dev-portfolio.netlify.app/")} className="grey-text text-lighten-3" href="#!">Hannah Price - Front End</Link></li>
+            </ul>
           </div>
         </div>
-        <div className="footer-copyright">
-          <div className="container">
-          © 2024 Copyright...
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-          </div>
+      </div>
+      <div className="footer-copyright">
+        <div className="container">
+          © 2024 Copyright
+          <Link onClick={()=> openInNewTab("https://github.com/mathilde-01/pixel-paws")} className="grey-text text-lighten-4 right" href="#!">Pixel Pals GitHub</Link>
         </div>
-      </footer>
-    )
+      </div>
+    </footer>
+  )
 }
