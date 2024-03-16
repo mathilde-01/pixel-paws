7af5d83a67e97b8c44f8aaa38670c257e4f7e427
Hannahphoto <hannahprice67@gmail.com>
2024-03-16 14:30:51 -0400

    alphabetical footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -22,8 +22,8 @@ export default function Footer() {
               <li><Link onClick={()=> openInNewTab("https://github.com/mathilde-01")}  className="grey-text text-lighten-3" href="#!">Mathilde Alacron - Back End</Link></li>
               <li><Link onClick={()=> openInNewTab("https://www.dianamariedischer.com/")}  className="grey-text text-lighten-3" href="#!">Diana Discher - Team Lead</Link></li>
               <li><Link onClick={()=> openInNewTab("https://github.com/scottd988")}  className="grey-text text-lighten-3" href="#!">Scott Denonn - Back End</Link></li>
-              <li><Link onClick={()=> openInNewTab("https://github.com/OpalSnellneberger")}  className="grey-text text-lighten-3" href="#!">Opal Snellenberger - Back End</Link></li>
               <li><Link onClick={()=> openInNewTab("https://hprice-web-dev-portfolio.netlify.app/")} className="grey-text text-lighten-3" href="#!" >Hannah Price - Front End</Link></li>
+              <li><Link onClick={()=> openInNewTab("https://github.com/OpalSnellneberger")}  className="grey-text text-lighten-3" href="#!">Opal Snellenberger - Back End</Link></li>
             </ul>
           </div>
         </div>
