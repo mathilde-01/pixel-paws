e2682b695ac05afbaffd921a748c79a52f972c88
Diana Discher <dianadischer@Dianas-MacBook-Pro.local>
2024-03-17 19:37:02 -0400

    added signin to auth.js and form page so that it redirects to /petcreation, added pw confirm display to page as well as show pw for signup and login, changed body bg color to match nav and footer

--- client/src/components/SignUp/SignUpForm.jsx
+++ client/src/components/SignUp/SignUpForm.jsx
@@ -1,8 +1,25 @@
 import '../../styles/style.css'
+import React, { useState } from 'react';
 
 export default function SignUpForm({ 
     submitHandler, changeHandler, emailState, passwordState, confirmPasswordState
 }) {
+
+    const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+    const handleConfirmPasswordChange = (event) => {
+        const confirmPwd = event.target.value;
+        const pwd = passwordState;
+        setPasswordsMatch(confirmPwd === pwd);
+        changeHandler(event);
+    }
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
 
         <div className="formContainer">
@@ -16,16 +33,24 @@ export default function SignUpForm({
                         </div>
                         <div>
                             <div className="input-field col s12">
-                                <input name="password" id="password" type="password" className="validate" value={passwordState} onChange={changeHandler}></input>
+                                <input name="password" id="password" type={showPassword ? 'text' : 'password'} className="validate" value={passwordState} onChange={changeHandler}></input>
                                 <label htmlFor="password">Password</label>
+                                
                             </div>
                         </div>
                         <div className="row">
                             <div className="input-field col s12">
-                                <input name= "confirmPassword" id="confirm-password" type="password" className="validate" value={confirmPasswordState} onChange={changeHandler}></input>
+                                <input name= "confirmPassword" id="confirm-password" type={showPassword ? 'text' : 'password'} className="validate" value={confirmPasswordState} onChange={handleConfirmPasswordChange}></input>
                                 <label htmlFor="confirm-password">Confirm Password</label>
+                                {!passwordsMatch && <p style={{color: 'red', fontSize: '12px', position: 'absolute', top: '40px'}}>Passwords don't match</p>}
                             </div>
                         </div>
+                    </div>
+                    <div className="row" style={{marginTop: '0', marginBottom: '20px'}}>
+                        <label>
+                            <input type="checkbox" onChange={togglePasswordVisibility} checked={showPassword} style={{marginRight: '2px'}} />
+                            <span style={{paddingLeft: '30px'}}>Show Password</span>
+                        </label>
                     </div>
                     <button className="btn" type="submit" name="action">SIGNUP</button>
                 </form>
--- server/models/Health.js
+++ server/models/Health.js
@@ -4,22 +4,22 @@ const healthSchema = new mongoose.Schema({
   fun: {
     type: Number,
     required: true,
-    default: 1
+    default: 0.4
   },
   cleanliness: {
     type: Number,
     required: true,
-    default: 1
+    default: 0.2
   },
   hunger: {
     type: Number,
     required: true,
-    default: 1
+    default: 0.6
   },
   sleep: {
     type: Number,
     required: true,
-    default: 1
+    default: 0.2
   }
 });
 
