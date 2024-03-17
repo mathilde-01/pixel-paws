b8dd414de6eec4c38f5ae8a9e5b6c5ed8ed26347
Hannahphoto <hannahprice67@gmail.com>
2024-03-17 19:03:18 -0400

    styled intro page, cant add animation to intro page

--- client/src/components/UserGuide.jsx
+++ client/src/components/UserGuide.jsx
@@ -1,15 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import ThreeScene from './ThreeScene';
+// import forest from '../assets/backgrounds/pet-backgrounds/forest.jpg'
+// import desert from '../assets/backgrounds/pet-backgrounds/desert.jpg'
+// import clouds from '../assets/backgrounds/pet-backgrounds/clouds.jpg'
+// import underwater from '../assets/backgrounds/pet-backgrounds/underwater.jpg'
+
 import '../styles/style.css'
 
-export default function UserGuide(){
-    return (
-    <div className='displayContainer'>
-      <h1 className='displayHeader'>Welcome to Pixel Pals!</h1>
-      <p className='guide-text'> Embark on a delightful journey with your very own Pixel Pal. In a world where every pixel counts, your companionship and care can turn these digital critters into your best pals. Nurture them, play fun games, and watch them grow. Are you ready to meet your new pal?</p>
-      <Link to="/signup" className='btn'>Get Started</Link>
-      <p className='guide-text'>Already signed up? Login Here:</p>
-      <Link to="/login" className='btn'>Login</Link>
-    </div>
-    )
+export default function UserGuide() {
+  // const [playAnimation, setPlayAnimation] = useState(false);
+  // const [cleanAnimation, setCleanAnimation] = useState(false);
+  // const [sleepAnimation, setSleepAnimation] = useState(false);
+  // const [feedAnimation, setFeedAnimation] = useState(false);
+
+  // let backgroundImage = '';
+  // switch (pet.location) {
+  //   case 'Forest':
+  //     backgroundImage = `url(${forest})`;
+  //     break;
+  //   case 'Desert':
+  //     backgroundImage = `url(${desert})`;
+  //     break;
+  //   case 'Clouds':
+  //     backgroundImage = `url(${clouds})`;
+  //     break;
+  //   case 'Underwater':
+  //     backgroundImage = `url(${underwater})`;
+  //     break;
+  //   default:
+  //     // Default background image or fallback
+  //     backgroundImage = `url(${forest})`;
+  //     break;
+  // }
+
+  return (
+    <>
+      <div className='card'>
+        <h1 className='displayHeader'>Welcome to Pixel Pals!</h1>
+        <p className='guide-text'> Embark on a delightful journey with your very own Pixel Pal. In a world where every pixel counts, your companionship and care can turn these digital critters into your best pals. Nurture them, play fun games, and watch them grow. Are you ready to meet your new pal?Here is how you play. 1. Adopt a pal. 2. Watch your Pal's health levels and take care of it within 48 hours, or you will have to adopt a new Pal.</p>
+        <Link to="/signup" className='btn' id="btnLeft">Get Started</Link>
+        <Link to="/login" className='btn' id="btnLeft">Login</Link>
+      </div>
+      {/* <div className="petContainer">
+        <div className="petCircle" style={{ backgroundImage }}>
+          <ThreeScene playAnimation={playAnimation} feedAnimation={feedAnimation} sleepAnimation={sleepAnimation} cleanAnimation={cleanAnimation} />
+        </div>
+        <div className="petStand"></div>
+      </div> */}
+    </>
+
+  )
 }
+
+
+
+
