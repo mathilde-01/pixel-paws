de5fb647adf9a8f2fb66c6e0ad842d4cbf188b2d
Diana Discher <dianadischer@Dianas-MacBook-Pro.local>
2024-03-17 23:53:35 -0400

    fixed layout bugs on display papage, altered userguide header and text layout

--- client/src/components/UserGuide.jsx
+++ client/src/components/UserGuide.jsx
@@ -8,17 +8,18 @@ export default function UserGuide() {
 
   return (
     <>
-     <h1 className='displayHeader'>Welcome to Pixel Pals!</h1>
+     <h1 className='userGuideHeader'>Welcome to Pixel Pals!</h1>
       <div className='card'>
-        <div className="petContainer">
-          {/* <div className=''></div> */}
+        <div className="imageContainer">
             <img src={pixelPal} alt="Example Pal" className='pixelPal'/> 
         </div>
         <div className='guide-container'>
           <p className='guide-text'> 
-            Embark on a delightful journey with your very own Pixel Pal. In a world where every pixel counts, your companionship and care can turn these digital critters into your best pals. Nurture them, play fun games, and watch them grow. Are you ready to meet your new pal?Here is how you play. 1. Adopt a pal. 2. Watch your Pal's health levels and take care of it within 48 hours, or you will have to adopt a new Pal.</p>
-          <Link to="/signup" className='btn'>Get Started</Link>
+            Embark on a delightful journey with your very own Pixel Pal. In a world where every pixel counts, your companionship and care can turn these digital critters into your best pals.<br/><br/>Are you ready to meet your new pal? Here is how to play: <br/>1. Adopt a pal- give it a name and a home {'<'}3 <br/>2. Watch your Pal's health levels and press the buttons to take care of it, or you will have to adopt a new Pal.</p>
+            <div>
+            <Link to="/signup" className='btn' style={{marginRight: '10px'}}>Get Started</Link>
           <Link to="/login" className='btn' >Login</Link>
+            </div>
         </div>
       </div>
     </>
