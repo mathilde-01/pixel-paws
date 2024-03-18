36b6c4d2907757c805a249a0931a7db97846754a
Diana Discher <dianadischer@Dianas-MacBook-Pro.local>
2024-03-18 01:17:57 -0400

    added adopt new pet? page that displays if any of the pet's bars reach 0. also changed which pages render depending on whether a user is logged in, or has created a pet

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 const ErrorPage = () => {
   return (
     <div className='displayContainer'>
-      <h1 className='displayHeader'>Oops!</h1>
       <h2 className='guide-text'>404 - Page Not Found</h2>
       <p className='guide-text'>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable. If you have an account, you can sign in below or you can create an account by signing up:</p>
-      <Link to="/signup" className='btn'>Get Started</Link>
-      <Link to="/login" className='btn'>Login</Link>
+      <div style={{display: 'flex', justifyContent: 'center', marginBottom: '80px'}}>
+        <Link to="/signup" className='btn' style={{marginRight: '10px'}}>Get Started</Link>
+        <Link to="/login" className='btn'>Login</Link>
+      </div>
     </div>
   );
 };
